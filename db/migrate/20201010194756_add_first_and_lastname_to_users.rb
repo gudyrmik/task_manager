1435ce7e315c9c5d34b97dd648a7314a79e8dc58
Mikhail Gudyrin <gudyrmik@gmail.com>
2020-10-10 22:09:54 +0200

    Added first/last name to registration process

---
+++ db/migrate/20201010194756_add_first_and_lastname_to_users.rb
@@ -0,0 +1,6 @@
+class AddFirstAndLastnameToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :firstname, :string, null: false
+    add_column :users, :lastname, :string, null: false
+  end
+end
