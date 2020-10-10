7677fc156a3f4d2cec76f06d5dfe1bcb64660bff
Mikhail Gudyrin <gudyrmik@gmail.com>
2020-10-10 23:10:03 +0200

    Added ActiveStorage

--- db/migrate/20201010201845_create_tags.rb
+++ db/migrate/20201010201845_create_tags.rb
@@ -2,7 +2,6 @@ class CreateTags < ActiveRecord::Migration[6.0]
   def change
     create_table :tags do |t|
       t.string :title, null: false
-      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
